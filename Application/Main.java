0eef34c365ce735d496421a47ee0c2c67521ca3d
Lucas Martins <lucasfranca_zs@hotmail.com>
2023-08-22 19:52:05 -0300

    Finalizando programa base

--- Application/Main.java
+++ Application/Main.java
@@ -20,25 +20,25 @@ public class Main {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		
-		System.out.print("Enter department's name:");
+		System.out.print("Enter department's name: ");
 		String departmentName = sc.nextLine();
 		
-		System.out.print("Enter worker data:");
+		System.out.println("Enter worker data:");
 		
-		System.out.println("Name: ");
+		System.out.print("Name: ");
 		String workerName = sc.nextLine();
-		System.out.println("Level: ");
+		System.out.print("Level: ");
 		String workerLevel = sc.nextLine();
-		System.out.println("Base Salary: ");
+		System.out.print("Base Salary: ");
 		double baseSalary = sc.nextDouble();
 		
 		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
 		
-		System.out.println("How many contracts to this worker?");
+		System.out.print("How many contracts to this worker? ");
 		int n = sc.nextInt();
 		
-		for(int i = 1; i < n; i++) {
-			System.out.println("Enter contract #" + i + "data");
+		for(int i = 1; i <= n; i++) {
+			System.out.println("Enter contract #" + i + " data");
 			
 			System.out.print("DATE (DD/MM/YYYY): ");
 			Date contractDate = sdf.parse(sc.next());
@@ -52,6 +52,16 @@ public class Main {
 			HourContract contract = new HourContract(contractDate, valuePerHour, hours);
 			worker.addContract(contract);
 		}
+		
+		System.out.println();
+		System.out.print("Enter month and year to calculate income (MM/YYYY): ");
+		String monthAndYear = sc.next();
+		int month = Integer.parseInt(monthAndYear.substring(0, 2));
+		int year = Integer.parseInt(monthAndYear.substring(3));
+		
+		System.out.println("Name: " + worker.getName());
+		System.out.println("Department: " + worker.getDepartment().getName());
+		System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
 		
 		
 		
